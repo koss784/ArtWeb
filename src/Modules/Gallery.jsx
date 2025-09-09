62c25a1a4e5388d06b18e5b21b91220cacebb130
koss <murloklane@gmail.com>
2025-09-09 12:10:19 +0300

    ClasslistNeeded

--- src/Modules/Gallery.jsx
+++ src/Modules/Gallery.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from 'react'
-
 const Gallery = () => {
-  const images = ['/img/Arctic.png', '/img/Dishonored.jpg', '/img/Ship.jpg']
+  const images2025 = import.meta.glob("/src/img/2025/" + "\x2A.{png,jpg,jpeg}", { eager: true, import: "default" });
+  const images2024 = import.meta.glob("/src/img/2024/" + "\x2A.{png,jpg,jpeg}", { eager: true, import: "default" });
+  const images2023 = import.meta.glob("/src/img/2023/" + "\x2A.{png,jpg,jpeg}", { eager: true, import: "default" });
+  const imageArray25 = Object.values(images2025)
+  const imageArray24 = Object.values(images2024)
+  const imageArray23 = Object.values(images2023)
+    console.log(images2025)
+console.log(Object.values(images2025))
   const [activeImg, setActiveImg] = useState(null)
 
   return (
@@ -10,14 +16,28 @@ const Gallery = () => {
       <div className='container bg-[url(/Fon.jpg)] absolute bg-cover bg-center max-w-screen h-screen'>
         <div className='w-full h-full backdrop-brightness-50 backdrop-blur-sm'></div>
       </div>
-      <div className=''>
-        <div className='absolute grid grid-flow-col auto-cols-max'>
-          {images.map((img) => (
+      <div className='absolute justify-center items-center w-screen'>
+        <div className='grid grid-cols-4 lg:grid-cols-6'>
+          {imageArray25.map((img) => (
             <img
               key={img}
-              className={`w-50 m-3 overflow-hidden framed shadow-2xl shadow-gray-950/50
-              ${img === '/img/Dishonored.jpg' ? "inset-y-5" : ''}
-              `}
+              className='lg:w-[25vh] md:w-[15vh] sm:w-[15vh] w-[12vh] m-3 overflow-hidden framed shadow-2xl shadow-gray-950/50'
+              src={img}
+              onClick={() => setActiveImg(img)}
+            />
+          ))}
+          {imageArray24.map((img) => (
+            <img
+              key={img}
+              className='hidden lg:w-[25vh] md:w-[15vh] sm:w-[15vh] w-[12vh] m-3 overflow-hidden framed shadow-2xl shadow-gray-950/50'
+              src={img}
+              onClick={() => setActiveImg(img)}
+            />
+          ))}
+          {imageArray23.map((img) => (
+            <img
+              key={img}
+              className='hidden lg:w-[25vh] md:w-[15vh] sm:w-[15vh] w-[12vh] m-3 overflow-hidden framed shadow-2xl shadow-gray-950/50'
               src={img}
               onClick={() => setActiveImg(img)}
             />
--- src/Modules/NavBar.jsx
+++ src/Modules/NavBar.jsx
@@ -1,36 +1,45 @@
-import React from 'react'
-const NavBar = () => {
-  const currentYear = new Date().getFullYear();
+  import React from 'react'
+  import imageArray2024 from './Gallery.jsx'
+  import { useState } from 'react';
+  const NavBar = () => {
+    const currentYear = new Date().getFullYear();
+    const [selectedYear, setSelectedYear] = useState(currentYear)
+    const years = [2023, 2024, 2025]
 
-  const years = [2023, 2024, 2025]
-
-  return (
-    <div className='container pb-2 pt-2 bg-gray-50 flex justify-center text-center max-w-screen h-full '>
-      <div>
-        <div className='text-orange-950 text-2xl font-bold pb-1.5'>
-          <p>Year</p>
+    return (
+      <>
+        <div className='container pb-2 pt-2 bg-gray-50 flex justify-center text-center max-w-screen h-full '>
+          <div>
+            <div className='text-orange-950 text-2xl font-bold pb-1.5'>
+              <p>Year</p>
+            </div>
+            <div className='justify-center items-center flex font-medium pb-2'>
+              {years.map((year) => (
+                <button
+                  key={year}
+                  type='button'
+                  className={`hover:bg-amber-400 hover:text-amber-950  mx-0.5 px-3 border-4 border-gray-500 rounded-b-2xl
+                    ${year === currentYear
+                      ? 'bg-gray-800 border-b-cyan-500 text-gray-100'
+                      : 'bg-gray-800 border-b-cyan-900 text-gray-100'}`
+                  }
+                  onClick={() => setSelectedYear(year)}
+                >
+                  {year}
+                </button>
+              ))}
+              
+            </div>
+          </div> 
         </div>
-        <div className='justify-center items-center flex font-medium pb-2'>
-          {years.map((year) => (
-            <button
-              key={year}
-              type='button'
-              className={`hover:bg-amber-400 hover:text-amber-950  mx-0.5 px-3 border-4 border-gray-500 rounded-b-2xl
-                ${year === currentYear
-                  ? 'bg-gray-800 border-b-cyan-500 text-gray-100'
-                  : 'bg-gray-800 border-b-cyan-900 text-gray-100'}`
-              }
-            >
-              {year}
-            </button>
-          ))}
+        <div id='Line1' className='relative bg-amber-500 max-w-screen h-2'></div> 
+        <div className=''>
+          {selectedYear === 2024 && (
+            imageArray2024. //classlist
+          )}
         </div>
-      </div>
-      <div className='my-20 absolute bg-amber-500 w-full h-5'></div>  
-      
-    </div>
-    
-  )
-}
+      </>
+    )
+  }
 
-export default NavBar
+  export default NavBar
